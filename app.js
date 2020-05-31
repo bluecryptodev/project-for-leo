2c4b7812d5cd84f7e33e88a04760ceccbb98f506
Pu chengxian <reactdev1995@yandex.com>
2020-06-01 00:52:55 +0800

    make it better

---
+++ app.js
@@ -0,0 +1,19 @@
+const express =require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = 5100;
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static('build'));
+app.get('/', (req, res) => {
+    res.send("hello")
+    res.sendFile(path.join(__dirname, 'build'));
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port: '+port)
+})
---
+++ build/precache-manifest.5609aac3d999db87529a451c0f12e90e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3466efd23a8dc4a5362559e8c4e1bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a90bbc1a16020d5a9b",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "70fb85ec3a9f7d71577c",
+    "url": "/static/css/main.81434c24.chunk.css"
+  },
+  {
+    "revision": "a6a90bbc1a16020d5a9b",
+    "url": "/static/js/2.a2eabb76.chunk.js"
+  },
+  {
+    "revision": "c527f6856b108f91426fceea77d6000d",
+    "url": "/static/js/2.a2eabb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70fb85ec3a9f7d71577c",
+    "url": "/static/js/main.9442e2cf.chunk.js"
+  },
+  {
+    "revision": "03f7e12cad7791e3bded",
+    "url": "/static/js/runtime-main.6ad67f0a.js"
+  },
+  {
+    "revision": "a900466063c371224101a1a125df9d56",
+    "url": "/static/media/Tang-Wenyuan-profile-photo.a9004660.jpg"
+  },
+  {
+    "revision": "e3a4639e1b86bc456fbd8641af9d0a91",
+    "url": "/static/media/Untitled-1.e3a4639e.png"
+  },
+  {
+    "revision": "4cd7a5ad7c8f47e64e25bbf59e9abcae",
+    "url": "/static/media/my-image.4cd7a5ad.jpg"
+  }
+]);
